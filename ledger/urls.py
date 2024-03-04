3b48cb6fa6a78ebf16f3385d0b8a1c998f5f8f27
astermangabat25 <157344930+astermangabat25@users.noreply.github.com>
2024-03-04 20:13:10 +0800

    Update urls.py

--- ledger/urls.py
+++ ledger/urls.py
@@ -3,7 +3,7 @@ from .views import RecipeListView, RecipeDetailView
 
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name="list"),
-    path('recipe/<int:pk>/detail', RecipeDetailView.as_view(), name="recipe")
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name="recipe")
 ]
 
 app_name = 'ledger'
