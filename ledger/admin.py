4e946bf966877d7b854e3e811d36818f55d85432
astermangabat25 <aster.mangabat@student.ateneo.edu>
2024-03-15 15:22:35 +0800

    Lab 3

--- ledger/admin.py
+++ ledger/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import Recipe, RecipeIngredient, Profile, Ingredient
+from .models import Recipe, RecipeIngredient, Profile
 # Register your models here.
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
