daea9045d95b581d8f18f112892f1d7ab3f04557
astermangabat25 <aster.mangabat@student.ateneo.edu>
2024-03-15 14:01:58 +0800

    Lab3

--- ledger/models.py
+++ ledger/models.py
@@ -40,5 +40,5 @@ class RecipeIngredient(models.Model):
 class Profile (models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    bio = models.CharField(max_length=255)
+    bio = models.TextField(max_length=255)
     
