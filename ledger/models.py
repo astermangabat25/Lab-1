3b27d36059e130ffc1e3f5615d5389fc9c0fe118
astermangabat25 <aster.mangabat@student.ateneo.edu>
2024-03-15 18:12:27 +0800

    Lab 3

--- ledger/models.py
+++ ledger/models.py
@@ -1,16 +1,10 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
-from django.core.validators import MinLengthValidator
+
 
 # Create your models here.
-class Profile (models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    bio = models.TextField(validators=[MinLengthValidator(255, 'the field must contain at least 255 characters')])
 
-    def __str__(self):
-        return self.name
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
@@ -24,7 +18,7 @@ class Ingredient(models.Model):
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
     author = models.ForeignKey(
-        Profile, 
+        User, 
         on_delete = models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
@@ -47,6 +41,3 @@ class RecipeIngredient(models.Model):
         on_delete = models.CASCADE,
         related_name = "ingredients"
     )
-
-
-    
